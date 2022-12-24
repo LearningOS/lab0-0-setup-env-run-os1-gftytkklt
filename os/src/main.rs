c79653b5c83474e666d9e91f881253930abeeccd
gftytkklt <100679322+gftytkklt@users.noreply.github.com>
2022-12-24 07:13:01 +0000

    copy os1 code exec

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,5 +1,88 @@
 #![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+use log::*;
+//use crate::sbi::shutdown;
+#[macro_use]
+mod console;
 mod lang_items;
-fn main() {
-    //println!("Hello, world!");
+mod sbi;
+mod logging;
+
+core::arch::global_asm!(include_str!("entry.asm"));
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret;
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+            inlateout("x10") args[0] => ret,
+            in("x11") args[1],
+            in("x12") args[2],
+            in("x17") id,
+        );
+    }
+    ret
 }
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
+fn clear_bss(){
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
+    clear_bss();
+    logging::init();
+    println!("Hello, world!");
+    trace!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    warn!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    error!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    panic!("Shutdown machine!");
+}
+/*pub fn rust_main() -> ! {
+    // print!("Hello, ");
+    // println!("world!");
+    // sys_exit(9);
+    clear_bss();
+    logging::init();
+    shutdown();
+}*/
+
+//fn main() {
+    //println!("Hello, world!");
+//}
+
